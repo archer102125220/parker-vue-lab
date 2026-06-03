c7b292f9d2a0188772dfac4b752cf7a2d8070941
Parker Chen <archer102125220.2015@gmail.com>
2026-06-03 13:01:47 +0800

    refactor: replace any types with generics and extend global Array interface for findLastIndex polyfill

--- src/utils/polyfill/array-find-last-index-polyfill.ts
+++ src/utils/polyfill/array-find-last-index-polyfill.ts
@@ -1,32 +1,32 @@
-export function findLastIndex(
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  array: Array<any>,
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  callback: (value: any, index: number, array: any[]) => unknown,
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  thisArg?: any
+declare global {
+  interface Array<T> {
+    findLastIndex(
+      predicate: (value: T, index: number, obj: T[]) => unknown,
+      thisArg?: unknown
+    ): number;
+  }
+}
+
+export function findLastIndex<T>(
+  array: T[],
+  callback: (this: unknown, value: T, index: number, array: T[]) => unknown,
+  thisArg?: unknown
 ): number {
   if (typeof callback?.call !== 'function') {
     throw new TypeError(`TypeError: ${typeof callback} is not a function`);
   }
   for (let index = array.length - 1; index >= 0; index--) {
-    if (callback.call(thisArg, array[index], index, array)) return index;
+    if (callback.call(thisArg, array[index] as T, index, array)) return index;
   }
   return -1;
 }
 
 export function handleFindLastIndexPolyfill(): void {
   if (typeof Array.prototype.findLastIndex !== 'function') {
-    Array.prototype.findLastIndex = function (
-      // TODO
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      callback: (value: any, index: number, array: any[]) => unknown,
-      // TODO
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      thisArg?: any
+    Array.prototype.findLastIndex = function <T>(
+      this: T[],
+      callback: (value: T, index: number, array: T[]) => unknown,
+      thisArg?: unknown
     ): number {
       return findLastIndex(this, callback, thisArg);
     };
